886e28155bf9a951bea8f7b5746b5a04642866b2
Sadmanul Hoque <sadmanulhoque21@gmail.com>
2025-06-05 20:43:49 +0600

    added frontend design

--- resources/views/user/vendor-request/index.blade.php
+++ resources/views/user/vendor-request/index.blade.php
@@ -1,3 +1,73 @@
+@extends('user.layout.master')
+@section('content')
+    <!-- Page header -->
+    <div class="page-header d-print-none">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <!-- Page pre-title -->
+
+                    <h2 class="page-title">
+                        Vendor Request
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page header -->
+    <!-- Page body -->
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="mb-4">Send Request To Be Vendor</h2>
+                        <h3 class="card-title">Profile Details</h3>
+                       <form action="{{ route('user.vendor-request.update', Auth::user()->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                         <div class="row align-items-center">
+                     
+                            <div class="form-label">Document</div>
+                            <div class="col-auto"> <input type="file" name="document" class="form-control"></div>
+                             <x-input-error :messages="$errors->get('document')" class="mt-2" />
+
+                        </div>
+                        <h3 class="card-title mt-4"></h3>
+                        <div class="row g-3">
+                            <div class="col-md">
+                                <div class="form-label">Contact</div>
+                                <input type="tel" name="contact" class="form-control">
+                                 <x-input-error :messages="$errors->get('contact')" class="mt-2 text-danger" />
+                            </div>
+                         
+
+                        </div>
+
+
+
+                        <button class="btn btn-primary mt-4">Send Request</button>
+                       </form>
+
+                    
+
+                         
+                 
+                    
+
+                     
+                         
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page body -->
+@endsection
+
+
+{{-- 
 <x-guest-layout>
     <form method="POST" action="{{ route('user.vendor-request.update', Auth::user()->id) }}" enctype="multipart/form-data">
         @csrf
@@ -36,3 +106,5 @@
         </div>
     </form>
 </x-guest-layout>
+
+--}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredVendorController;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\UserDashboardController;
 use App\Http\Controllers\User\UserProfileController;
@@ -10,36 +11,51 @@ use App\Http\Controllers\Vendor\VendorDashboardController;
 use App\Http\Controllers\Vendor\VendorProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 
-    Route::get('vendor/register', [RegisteredVendorController::class, 'create'])->name('vendor.register.index');
-    Route::post('vendor/register/store', [RegisteredVendorController::class, 'store'])->name('vendor.register.store');
 
-Route::group(['middleware' => ['auth', 'verified', 'check_role:user'], 'prefix' => 'user', 'as' => 'user.'], function(){
+Route::get('vendor/register', [RegisteredVendorController::class, 'create'])->name('vendor.register.index');
+Route::post('vendor/register/store', [RegisteredVendorController::class, 'store'])->name('vendor.register.store');
+
+/**
+ * User Routes
+ */
+Route::group(['middleware' => ['auth', 'verified', 'check_role:user'], 'prefix' => 'user', 'as' => 'user.'], function () {
 
     Route::resource('profile', UserProfileController::class);
     Route::resource('vendor-request', UserVendorRequestController::class);
-
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
-    
 });
+/**
+ * User Routes
+ */
 
-Route::group(['middleware' => ['auth', 'verified', 'check_role:vendor'], 'prefix' => 'vendor', 'as' => 'vendor.'], function(){
+
+ /**
+ * Vendor Routes
+ */
+Route::group(['middleware' => ['auth', 'verified', 'check_role:vendor'], 'prefix' => 'vendor', 'as' => 'vendor.'], function () {
 
     Route::resource('profile', VendorProfileController::class);
-
     Route::get('dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
 });
+ /**
+ * Vendor Routes
+ */
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
-require __DIR__.'/admin.php';
+ /**
+ * Frontend Routes
+ */
+Route::get('/', [HomeController::class, 'home'])->name('home');
+
+  /**
+ * Frontend Routes
+ */
+
+
+
+
+require __DIR__ . '/auth.php';
+require __DIR__ . '/admin.php';
